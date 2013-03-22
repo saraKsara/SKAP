77551010126f2ebfee4a5fc8e67fd9722d12e31c
Asa Persson <asamaripersson@gmail.com>
2013-03-22 15:21:42 +0100

    adds addBabyVc

---
+++ SLKAddBabyViewController.m
@@ -0,0 +1,38 @@
+//
+//  SLKAddBabyViewController.m
+//  SKAP
+//
+//  Created by Åsa Persson on 2013-03-22.
+//  Copyright (c) 2013 Student vid Yrkeshögskola C3L. All rights reserved.
+//
+
+#import "SLKAddBabyViewController.h"
+
+@interface SLKAddBabyViewController ()
+
+@end
+
+@implementation SLKAddBabyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
