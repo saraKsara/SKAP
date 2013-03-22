298c6e6a5c7a2dbd25f65b5542d550e4573897d7
Asa Persson <asamaripersson@gmail.com>
2013-03-22 09:36:02 +0100

    adds afnetworking files

---
+++ SKAP/SLKJSONService.m
@@ -0,0 +1,13 @@
+//
+//  SLKJSONService.m
+//  SKAP
+//
+//  Created by Åsa Persson on 2013-03-22.
+//  Copyright (c) 2013 Student vid Yrkeshögskola C3L. All rights reserved.
+//
+
+#import "SLKJSONService.h"
+
+@implementation SLKJSONService
+
+@end
