ecc8d8c043e7ca8b7dca96dde48ddffa18650eca
Asa Persson <asamaripersson@gmail.com>
2013-04-04 09:29:39 +0200

    adds setting vc and cell

---
+++ SLKSettingCell.h
@@ -0,0 +1,13 @@
+//
+//  SLKSettingCell.h
+//  SKAP
+//
+//  Created by Åsa Persson on 2013-04-04.
+//  Copyright (c) 2013 Student vid Yrkeshögskola C3L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLKSettingCell : UITableViewCell
+
+@end
---
+++ SLKSettingsViewController.m
@@ -0,0 +1,43 @@
+//
+//  SLKSettingsViewController.m
+//  SKAP
+//
+//  Created by Åsa Persson on 2013-04-04.
+//  Copyright (c) 2013 Student vid Yrkeshögskola C3L. All rights reserved.
+//
+
+#import "SLKSettingsViewController.h"
+
+@interface SLKSettingsViewController ()
+
+@end
+
+@implementation SLKSettingsViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
