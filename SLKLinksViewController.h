6a80eab6df9a38448793a04fb703f5b47077fbc8
Asa Persson <asamaripersson@gmail.com>
2013-04-08 14:20:06 +0200

    kills auto layout

---
+++ SLKLinksViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLKLinksViewController.h
+//  SKAP
+//
+//  Created by Åsa Persson on 2013-04-08.
+//  Copyright (c) 2013 Student vid Yrkeshögskola C3L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLKLinksViewController : UITableViewController
+
+@end
