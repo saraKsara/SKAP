eb435064cf9cbd7ea918659bcd0cb7dcc141c96a
sara krantz <sarali.krantz@gmail.com>
2013-04-08 10:42:10 +0200

    pageView in FoodViewController

--- SLKFoodViewController.m
+++ SLKFoodViewController.m
@@ -30,6 +30,7 @@
 
 @implementation SLKFoodViewController
 {
+   
     float bottledFood;
     SLKBabyListTableViewController *settingsVC;
     float checkDirection;
@@ -40,7 +41,10 @@
     int numberOfBabies;
     NSArray *babyArray;
     float segmentWidth;
+    NSNull *nullValue;
 }
+
+
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     
@@ -67,22 +71,106 @@
 
 -(void)viewWillAppear:(BOOL)animated
 {
-    [super viewWillAppear:YES];
+    [super viewWillAppear:animated];
+    nullValue = [NSNull null];
+    // Set up the content size of the scroll view
+    CGSize pagesScrollViewSize = self.scrollView.frame.size;
+    self.scrollView.contentSize = CGSizeMake(pagesScrollViewSize.width * self.pageViews.count, pagesScrollViewSize.height);
+   
+    // Load the initial set of pages that are on screen
+   
     //TODO: move to set up class???
      [_segmentControll setSelected:NO];
-    
+     
+    _scrollView.delegate = self;
+     [_scrollView setScrollEnabled:YES];
     self.view.backgroundColor = [UIColor clearColor];
-
+    
+    
     
     checkDirection = 30;
     date = [NSDate date];
     time = [SLKDateUtil formatTimeFromDate:date];
     _setTimeLabel.text = time;
+     [self loadVisiblePages];
 }
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-//    
+    self.pageViews = [NSMutableArray arrayWithObjects:_bottleView, _breastView, nil];
+    NSInteger pageCount = self.pageViews.count;
+    
+    self.pageControll.currentPage = 0;
+    self.pageControll.numberOfPages = pageCount;
+    
+    CGAffineTransform trans = CGAffineTransformMakeRotation(M_PI * 1.5);
+    _foodSliderOne.transform = trans;
+    currentBabe = [[SLKBabyStorage sharedStorage] getCurrentBaby];
+    _anotherFoodLable.text = [NSString stringWithFormat: @"%@´s bottled meal", currentBabe.name];
+    [_anotherFoodLable sizeToFit];
+    bottledFood = _foodSliderOne.value;
+    _foodLabel.text = [NSString stringWithFormat:@"%.f",bottledFood];
+    NSLog(@"slider ONE: %f ", [_foodSliderOne value]);
+}
+- (void)loadVisiblePages {
+    // First, determine which page is currently visible
+    CGFloat pageWidth = self.scrollView.frame.size.width;
+    NSInteger page = (NSInteger)floor((self.scrollView.contentOffset.x * 2.0f + pageWidth) / (pageWidth * 2.0f));
+    
+    // Update the page control
+    self.pageControll.currentPage = page;
+    
+    // Work out which pages we want to load
+    NSInteger firstPage = page - 1;
+    NSInteger lastPage = page + 1;
+    
+    // Purge anything before the first page
+    for (NSInteger i=0; i<firstPage; i++) {
+        [self purgePage:i];
+    }
+    for (NSInteger i=firstPage; i<=lastPage; i++) {
+        [self loadPage:i];
+    }
+    for (NSInteger i=lastPage+1; i<self.pageViews.count; i++) {
+        [self purgePage:i];
+    }
+}
+- (void)loadPage:(NSInteger)page {
+    if (page < 0 || page >= self.pageViews.count) {
+        // If it's outside the range of what we have to display, then do nothing
+        return;
+    }
+    
+    // Load an individual page, first seeing if we've already loaded it
+    UIView *pageView = [self.pageViews objectAtIndex:page];
+    if (pageView == nil) {
+        CGRect frame = self.scrollView.bounds;
+        frame.origin.x = frame.size.width * page;
+        frame.origin.y = 0.0f;
+        
+        UIView *newPageView = [[UIView alloc] init];
+        newPageView =[self.pageViews objectAtIndex:page];
+        newPageView.contentMode = UIViewContentModeScaleAspectFit;
+        newPageView.frame = frame;
+        [self.scrollView addSubview:newPageView];
+        [self.pageViews replaceObjectAtIndex:page withObject:newPageView];
+    }
+}
+
+- (void)purgePage:(NSInteger)page {
+    if (page < 0 || page >= self.pageViews.count) {
+       
+        return;
+    }
+    
+    // Remove a page from the scroll view and reset the pagearray
+    UIView *pageView = [self.pageViews objectAtIndex:page];
+    if (pageView == nil) {
+        [pageView removeFromSuperview];
+        [self.pageViews replaceObjectAtIndex:page withObject:nullValue];
+    }
+}
 //    settingsVC = [[SLKBabyListTableViewController alloc] init];
 //    
 //    babyArray = [[SLKBabyStorage sharedStorage] babyArray];
@@ -129,18 +217,15 @@
 //       [_segmentControll setHighlighted:NO];
 //       [_segmentControll setTintColor:[UIColor clearColor]];
     
-    CGAffineTransform trans = CGAffineTransformMakeRotation(M_PI * 1.5);
-    _foodSliderOne.transform = trans;
-    currentBabe = [[SLKBabyStorage sharedStorage] getCurrentBaby];
-    _anotherFoodLable.text = [NSString stringWithFormat: @"Log how much milk substitute %@ ate", currentBabe.name];
-    [_anotherFoodLable sizeToFit];
-    bottledFood = _foodSliderOne.value;
-    _foodLabel.text = [NSString stringWithFormat:@"%.f",bottledFood];
-   NSLog(@"slider ONE: %f ", [_foodSliderOne value]);
+  
 
   // }
     
-}
+
+
+
+
+
 
 //-(UIImage*)drawImageWithColor:(UIColor*)color
 //{
@@ -272,4 +357,31 @@
     time = [SLKDateUtil formatTimeFromDate:date];
     _setTimeLabel.text = time;
 }
+
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    
+    self.scrollView = nil;
+    self.pageControll = nil;
+    self.pageViews = nil;
+}
+
+- (IBAction)valuePageControll:(id)sender;
+{
+    [self loadVisiblePages];
+
+}
+
+- (IBAction)touchUpInsidePageControll:(id)sender
+{
+    
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    [self loadVisiblePages];
+}
+
+
 @end
